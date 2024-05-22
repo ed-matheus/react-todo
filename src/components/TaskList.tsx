5ee11bacc1686ec489d589ecfcd487342b98a65d
ed-matheus <edsonmatheus02@hotmail.com>
2024-05-22 18:10:37 -0300

    testando tarefas e configurando lista

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 
-type Props = {}
+// Interfaces
+import { ITask } from '../interfaces/Task'
 
-const TaskList = (props: Props) => {
+// CSS
+import styles from './TaskList.module.css'
+
+type Props = {
+    taskList: ITask[]
+}
+
+const TaskList = ({taskList}: Props) => {
     return (
-        <div>Task List</div>
+        <>
+            {taskList.length > 0 ? (
+                taskList.map((task) => (
+                    <div key={task.id} className={styles.task}>
+                        <div className={styles.details}>
+                            <h4>{task.title}</h4>
+                            <p>Dificuldade: {task.difficulty}</p>
+                        </div>
+                        <div className={styles.actions}>
+                            <i className='bi bi-pencil'></i>
+                            <i className='bi bi-trash'></i>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <p>Não há tarefas cadastradas</p>
+            )}
+        </>
     )
 }
 
