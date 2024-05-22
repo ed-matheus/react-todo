366e4088af9602dfca8c454ce7461f9fcc643af9
ed-matheus <edsonmatheus02@hotmail.com>
2024-05-22 17:12:02 -0300

    primeiros estilos no header e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+
+// Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
+// CSS
+import styles from './App.module.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <h1>Conteúdo...</h1>
+        <main className={styles.main}>
+          <h1>Conteúdo...</h1>
+        </main>
       <Footer />
     </div>
   );
