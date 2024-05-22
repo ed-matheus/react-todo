e28e8811a5d4f7dca12e2b3298fc00b5445700ec
ed-matheus <edsonmatheus02@hotmail.com>
2024-05-22 17:55:04 -0300

    ações do formulário

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 // Components
 import Header from "./components/Header";
@@ -9,14 +9,23 @@ import TaskList from "./components/TaskList";
 // CSS
 import styles from './App.module.css'
 
+// Interface
+import { ITask } from "./interfaces/Task";
+
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([])
+
   return (
     <div className="App">
       <Header />
         <main className={styles.main}>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText="Criar Tarefa" />
+          <TaskForm
+            btnText="Criar Tarefa"
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
           </div>
         <div>
           <h2>Suas tarefas:</h2>
